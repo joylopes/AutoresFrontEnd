bca29594b7436ac3740c01531491aba195f55253
Joice Lopes <junior@techdog.com.br>
2020-03-15 15:46:31 -0300

    listar autor concluído

--- ProjetoAutores/src/app/Autores/listar-autores/listar-autores.component.ts
+++ ProjetoAutores/src/app/Autores/listar-autores/listar-autores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutorService } from 'src/app/Services/Autor/autor.service';
 
 @Component({
   selector: 'app-listar-autores',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarAutoresComponent implements OnInit {
 
-  constructor() { }
+  listaDeAutoresNome: any[] = [];
+  constructor(
+    private autorService: AutorService
+  ) {
+    this.autorService.lstDeNomes.subscribe(x => {
+      this.listaDeAutoresNome = x;
+    });
+  }
 
   ngOnInit() {
   }
